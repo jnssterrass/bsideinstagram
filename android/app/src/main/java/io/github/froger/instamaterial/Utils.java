8067ba9fab5e460f527e392124b573f5c3829660
juan.salmeron <jnssterrass@gmail.com>
2018-09-15 21:03:54 +0200

    vision roto

--- android/app/src/main/java/io/github/froger/instamaterial/Utils.java
+++ android/app/src/main/java/io/github/froger/instamaterial/Utils.java
@@ -2,10 +2,18 @@ package io.github.froger.instamaterial;
 
 import android.content.Context;
 import android.content.res.Resources;
+import android.graphics.Path;
 import android.graphics.Point;
+import android.media.Image;
 import android.os.Build;
+import android.provider.MediaStore;
 import android.view.Display;
 import android.view.WindowManager;
+
+import com.fasterxml.jackson.core.JsonParser;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by froger_mcs on 05.11.14.
@@ -13,6 +21,46 @@ import android.view.WindowManager;
 public class Utils {
     private static int screenWidth = 0;
     private static int screenHeight = 0;
+
+    public static void main(String... args) throws Exception {
+        // Instantiates a client
+        try (ImageAnnotatorClient vision = ImageAnnotatorClient.create()) {
+
+            // The path to the image file to annotate
+            String fileName = "./resources/wakeupcat.jpg";
+
+            // Reads the image file into memory
+            Path path = paths.get(fileName);
+            byte[] data = MediaStore.Files.readAllBytes(path);
+            ByteString imgBytes = ByteString.copyFrom(data);
+
+            // Builds the image annotation request
+            List<AnnotateImageRequest> requests = new ArrayList<>();
+            Image img = Image.newBuilder().setContent(imgBytes).build();
+            JsonParser.Feature feat = Feature.newBuilder().setType(Type.LABEL_DETECTION).build();
+            AnnotateImageRequest request = AnnotateImageRequest.newBuilder()
+                    .addFeatures(feat)
+                    .setImage(img)
+                    .build();
+            requests.add(request);
+
+            // Performs label detection on the image file
+            BatchAnnotateImagesResponse response = vision.batchAnnotateImages(requests);
+            List<AnnotateImageResponse> responses = response.getResponsesList();
+
+            for (AnnotateImageResponse res : responses) {
+                if (res.hasError()) {
+                    System.out.printf("Error: %s\n", res.getError().getMessage());
+                    return;
+                }
+
+                for (EntityAnnotation annotation : res.getLabelAnnotationsList()) {
+                    annotation.getAllFields().forEach((k, v) ->
+                            System.out.printf("%s : %s\n", k, v.toString()));
+                }
+            }
+        }
+    }
 
     public static int dpToPx(int dp) {
         return (int) (dp * Resources.getSystem().getDisplayMetrics().density);

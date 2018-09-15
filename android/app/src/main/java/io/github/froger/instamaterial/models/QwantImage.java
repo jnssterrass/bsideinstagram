8ec793435abd9de4bef1d03ef42f9284aa8719ce
elorenteg <ester.lorente@est.fib.upc.edu>
2018-09-15 20:34:52 +0200

    Integrate Qwant Image Search

---
+++ android/app/src/main/java/io/github/froger/instamaterial/models/QwantImage.java
@@ -0,0 +1,13 @@
+package io.github.froger.instamaterial.models;
+
+public class QwantImage {
+    private String media;
+
+    public String getMedia() {
+        return media;
+    }
+
+    public void setMedia(String media) {
+        this.media = media;
+    }
+}

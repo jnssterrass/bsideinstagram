a97e6bb026e41244870c2c36fa23e6ee7fdf540e
juan.salmeron <jnssterrass@gmail.com>
2018-09-16 09:35:46 +0200

    Created newsapi class

---
+++ android/app/src/main/java/io/github/froger/instamaterial/ui/activity/NewsApi.java
@@ -0,0 +1,13 @@
+package io.github.froger.instamaterial.ui.activity;
+
+/**
+ * Created by juan on 16/09/2018.
+ */
+
+public class NewsApi {
+    String NEWSAPI_TOKEN = "";
+    String COUNTRY = "es";
+
+    String NEWSAPI_BASE_URL = "https://newsapi.org/v2/top-headlines?country="+COUNTRY+"&apiKey="+NEWSAPI_TOKEN;
+
+}
